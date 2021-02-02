194384252a68d0bedcc9a7a8c54b6fdece8e719c
Angga Afrianto <anggaaurelius@gmail.com>
2021-02-02 17:25:10 +0700

    coba express.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 5000;
+
+app.use(bodyParser.json());
+
+app.get("/", (request, response) => {
+    response.send("Hello Dumbways ok");
+});
+
+app.get("/todos", (req, res) => {
+    res.send({
+        message: "Response Succes",
+        data: {
+            todos,
+
+        },
+    });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}, App Ready !`);
+});
+
+const todos = [
+    {
+        id: 1,
+        title: "Belajar express.js",
+        isDone: true,
+    },
+    {
+        id: 2,
+        title: "Belajar Node.js",
+        isDone: false,
+    },
+];
+
