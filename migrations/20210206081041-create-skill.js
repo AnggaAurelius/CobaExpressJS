6d0bbeacdbfc4f9f7f2c93e58e345c52fa05e7e2
Angga Afrianto <anggaaurelius@gmail.com>
2021-02-06 15:27:26 +0700

    table programer and skill (belongsTo programer)

---
+++ migrations/20210206081041-create-skill.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Skills', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      programmerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "programmers",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Skills');
+  }
+};
