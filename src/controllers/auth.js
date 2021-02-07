2e6300cbd095567c962458e056ef78827479b571
Angga Afrianto <anggaaurelius@gmail.com>
2021-02-07 12:50:29 +0700

    register and token

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,6 +5,8 @@ const joi = require("joi");
 
 exports.register = async (req, res) => {
     try {
+        const { email, password } = req.body;
+        
         const schema = joi.object({
             fullName: joi.string().min(3).required(),
             email: joi.string().email().min(6).required(),
@@ -17,6 +19,32 @@ exports.register = async (req, res) => {
             return res.status(400).send({
                 message: error.details[0].message,
             })
+        const hashedStrenght = 10;
+        const hashedPassword = await bcrypt.hash(password, hashedStrenght);
+
+        const user = await User.create({
+            ...req.body,
+            password: hashedPassword,
+        });
+
+        const secretKey = "your-secret";
+        const token = jwt.sign(
+            {
+            id: user.id
+            },
+            secretKey
+        );
+
+        res.send({
+            status: "succes",
+            message: "Register Succces",
+            data: {
+                user: {
+                    email,
+                    token, 
+                },
+            },
+        });
     } catch (err) {
         console.log(err);
         res.status(500).send({
